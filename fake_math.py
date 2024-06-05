9df104d6e048016385cd0096bd91347c299daf59
Ann109 <andersonhloa@gmail.com>
2024-06-05 16:24:37 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,18 @@
+def fake_divide(first, second):
+        if second == 0:
+                return 'Ошибка'
+        else:
+                return str(first / second)
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ true_math.py
@@ -0,0 +1,9 @@
+def true_divide(first, second):
+        from math import inf
+        if second == 0:
+                return inf
+        else:
+                return str(first / second)
+
+
+
